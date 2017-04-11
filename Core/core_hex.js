f9f453af6bec8294c8e2cdb678f05a10c6cbe1ff
Cristian Radu <Cristian Radu>
2017-04-11 10:16:41 +0300

    updates in core_hex

--- Core/core_hex.js
+++ Core/core_hex.js
@@ -1,19 +1,25 @@
 'use strict'
 const log = require('winston');
 
+/**
+ * @param {String} i unique ID, ussually map index
+ * 
+ * @class Hex
+ */
 class Hex {
-    constructor ()
+    constructor (i)
     {
+       this. id = i;
         // vertexes start with top-most one (North) as vertex0 and continues clockwise
-        Vertexes: ['vertex0', 'vertex1', 'vertex2', 'vertex3', 'vertex4', 'vertex5'];
+        this.Vertexes = ['vertex0', 'vertex1', 'vertex2', 'vertex3', 'vertex4', 'vertex5'];
         // edge0 = vertex0-vertex1, edge1 = vertex1-vertex2, edge2 = vertex2-vertex3, edge3 = vertex3-vertex4, edge4 = vertex4-vertex5, edge5 = vertex5-vertex0
-        Edges: ['edge0', 'edge1', 'edge2', 'edge3', 'edge4', 'edge5'];
+        this.Edges = ['edge0', 'edge1', 'edge2', 'edge3', 'edge4', 'edge5'];
         // array of objects describing other hex attributes (NOT items placed on top of the Hex)
-        Contents: [];
+        this.Contents = [];
     }
     
     /**
-     * Returns this.Vertexs[n]
+     * Returns this.Vertexes[n]
      * 
      * @param {Number} n between 1-6
      * @returns {String} Vertex id
