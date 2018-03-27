021d3e192647a9d3c311c2c0c88356f0db233437
Radu Cristian <cristi.radu@gmail.com>
2018-03-27 12:34:43 +0300

    work

--- 1830webapp/src/Store/1830store.js
+++ 1830webapp/src/Store/1830store.js
@@ -1,20 +1,27 @@
 import { types } from "mobx-state-tree";
 import Shares from "../1830/1830shares";
 
-const PublicCompany = types.model("PublicCompany", {
-  PCname: types.string,
-  PCShares: types.number
+const PrivateCompany = types.model("PrivateCompany", {
+  PCname: types.maybe(types.string),
+  PCShares: types.maybe(types.number)
 });
 
-const PublicCompanies = types.model("PublicCompanies", {
-  PCompaniesArray: types.array(PublicCompany)
+const PrivateCompanies = types.model("PrivateCompanies", {
+  PCompaniesArray: types.array(PrivateCompany)
 })
-.create({
-  PCompaniesArray : Shares.PCompanies1830
-})
+.actions(self => ({
+  addComp: (Comp) => {
+      self.PrivateCompanies.push(Comp)
+    } 
+}))
 
 const Store1830 = types.model("Store1830", {
-  PCompanies: PublicCompanies
-});
+  PCompanies: PrivateCompanies
+})
+
+const names = Shares.PCompanies1830.PCompanies1830.map(x=>x.Id)
+const PrivateComp = PrivateCompany.create({PCname:"", PCShares:0})
+// const Store = PrivateCompanies.create(PrivateComp)
+// names.forEach((x)=>{Store.addComp({PCname:x, PCShares:0 })})
 
 export default Store1830;
